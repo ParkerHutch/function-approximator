58f7822a0480b7a5c37828c5904dcadbdde708aa
ParkerHutch <parker.hutch@outlook.com>
2020-04-05 21:33:51 -0400

    Refined code

--- program.py
+++ program.py
@@ -15,10 +15,11 @@ def noise(variance):
     return random.randrange(-variance, variance)
     
 def target_function(x):
-    return 5 * x + 2
+    return 2 * x
+    #return 5 * x + 2 + noise(2)
 
 df = pd.DataFrame()
-df['X'] = [x for x in range(0, 100000)]
+df['X'] = [x for x in range(0, 1000)]
 df['Y'] = df['X'].apply(target_function)
 
 """ DATA PREPROCESSING """ 
@@ -31,7 +32,8 @@ TODO: Do I really need normalization for this?
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 df['X'] = scaler.fit_transform(df[['X']])
-df['Y'] = scaler.fit_transform(df[['Y']])"""
+df['Y'] = scaler.fit_transform(df[['Y']])
+"""
 
 """ MODEL CREATION """
 def build_model(num_layers, num_hidden_units):
@@ -44,40 +46,22 @@ def build_model(num_layers, num_hidden_units):
 
 # Simple model: build_model(1, 2)
 model = build_model(2, 50)
-"""
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(50, activation='relu'),
-    tf.keras.layers.Dense(50, activation='relu'),
-    tf.keras.layers.Dense(1)
-    ])
-"""
 
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-# Try different optimizers: SGD goes to infinity
+# Try different optimizers: with SGD, loss goes to infinity
 
 num_epochs=20
-early_stopper = tf.keras.callbacks.EarlyStopping(monitor='loss', 
-                                                 mode='min', patience=3)
+early_stopper = tf.keras.callbacks.EarlyStopping(monitor='mae', 
+                                                 mode='min', patience=2)
+
 model.fit(df['X'].values, df['Y'].values, epochs=num_epochs, 
           callbacks=[early_stopper])
 
-
-
 """ EVALUATION """
-cleaned_y = []
-for val in preds:
-    for cleaned_val in val:
-        cleaned_y.append(cleaned_val)
-        
-"""
-for element in train_dataset.as_numpy_iterator(): 
-    for val in element:
-        print(val)"""
-        
+preds = [pred[0] for pred in model.predict(df['X']).tolist()]
+
 import seaborn as sns
 import matplotlib.pyplot as plt
-ax = sns.lineplot(x=data['X'], y=cleaned_y)
-ax = sns.lineplot(x=data['X'], y=data['Y'])
-
-
+ax = sns.lineplot(x=df['X'][:100], y=preds[:100])
+ax = sns.lineplot(x=df['X'][:100], y=df['Y'][:100])
 
